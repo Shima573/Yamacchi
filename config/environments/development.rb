0300f48d6cdba065f8e7a7c21fb9190b72518ae9
島拓海 <shimatakumi@shimatakuminoiMac.local>
2026-05-03 21:37:19 +0900

    パスワードリセットメール（Resend）送信確認

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,10 +48,11 @@ Rails.application.configure do
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :resend
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000, protocol: 'http' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
